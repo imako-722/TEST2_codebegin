673dea85fa1701fca79ffc1c1a06424edb99f83f
imako <imako.0722@gmail.com>
2020-09-18 22:32:35 +0900

    add discription,change detail

--- js/script.js
+++ js/script.js
@@ -3,16 +3,17 @@ $(function () {
   $(".slick-box").slick({
     accessibility: true,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 2500,
     fade: true,
     dots: false,
     arrows: false,
   });
+  // ハンバーガーメニュー
   $("#humberger-btn").click(function () {
     $(this).toggleClass("active");
     $("#humberger-nav").toggleClass("h-nav-active");
   });
-
+  // 上に戻るボタン
   var pagetop = $("#page_top");
   // ボタン非表示
   pagetop.hide();
